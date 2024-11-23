741b919ba009e0ad48dc06306adc48c3c38118b0
nadavpiv <nadavpivonia1234567@gmail.com>
2024-11-23 18:44:52 +0200

    with start

---
+++ vacationPlanner/src/test/java/com/nadavpiv/vacationPlanner/VacationPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadavpiv.vacationPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
