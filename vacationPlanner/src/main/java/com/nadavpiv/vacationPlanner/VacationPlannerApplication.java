aa9d73ee8ba3ac2c06ae550ce1a92d1cb5d089a9
nadavpiv <nadavpivonia1234567@gmail.com>
2024-11-23 18:40:25 +0200

    with start

--- vacationPlanner/src/main/java/com/nadavpiv/vacationPlanner/VacationPlannerApplication.java
+++ vacationPlanner/src/main/java/com/nadavpiv/vacationPlanner/VacationPlannerApplication.java
@@ -3,7 +3,7 @@ package com.nadavpiv.vacationPlanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//
 public class VacationPlannerApplication {
 
 	public static void main(String[] args) {
